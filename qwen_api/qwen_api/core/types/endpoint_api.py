f845e6dc05891bc4e427124f6d81c9bcc0868ffe
stas <stas-fomin@yandex.ru>
2025-09-29 11:12:31 +0300

    Since 2025-09-29 Qwen close unversioned endpoints, and we need v2 paths according to chats

--- qwen_api/qwen_api/core/types/endpoint_api.py
+++ qwen_api/qwen_api/core/types/endpoint_api.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class EndpointAPI:
     new_chat: str = "/api/v1/chats/new"
-    completions: str = "/api/chat/completions"
-    completed: str = "/api/chat/completed"
-    suggestions: str = "/api/task/suggestions/completions"
+    completions: str = "/api/v2/chat/completions"
+    completed: str = "/api/v2/chat/completed"
+    suggestions: str = "/api/v2/task/suggestions/completions"
     upload_file: str = "/api/v1/files/getstsToken"
